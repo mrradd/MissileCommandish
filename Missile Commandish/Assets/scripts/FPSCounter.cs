cce200fceb8e32a7ae9f5062681f387bceae628f
mrradd <conrad.horton@gmail.com>
2019-07-07 19:27:51 -0700

    Added fps counter and modified particles for fire.
    This greatly affected fps. Next replace fire particles with animation.

---
+++ Missile Commandish/Assets/scripts/FPSCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+  {
+  float frameCount = 0;
+  float dt = 0;
+  int fps = 0;
+  float updateRate = 4;  // 4 updates per sec.
+
+  public Text fpsText;
+
+  void Update()
+    {
+    frameCount++;
+    dt += Time.deltaTime;
+    if(dt > 1.0f / updateRate)
+      {
+      fps = (int)Math.Floor(frameCount / dt);
+      fpsText.text = "FPS " + fps;
+      frameCount = 0f;
+      dt -= 1.0f / updateRate;
+      }
+    }
+  }
